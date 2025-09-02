aeed7403ec808994588a135ac1c2e4ef5e7415a2
Efim Nushtaev <enushtaev@alfabank.ru>
2025-09-02 21:43:00 +0300

    add backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [timestamp, setTimestamp] = useState('');
+
+  useEffect(() => {
+    const fetchTimestamp = () => {
+      fetch('/api/getTimestamp')
+        .then(response => response.json())
+        .then(data => setTimestamp(data.timestamp));
+    };
+
+    fetchTimestamp();
+    const interval = setInterval(fetchTimestamp, 10000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload 4.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Текущее время сервера:</h1>
+      <p>{timestamp || 'Загрузка...'}</p>
     </div>
   );
 }
